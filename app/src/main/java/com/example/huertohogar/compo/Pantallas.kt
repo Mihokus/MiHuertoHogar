7e1b1cd213b2e369f0de99b1619301421d896f6e
Mihokus <seba.rodriguezg@duocuc.cl>
2025-11-01 00:18:29 -0300

    Inicio de sesion exitoso, cambia de pantalla principal

---
+++ app/src/main/java/com/example/huertohogar/compo/Pantallas.kt
@@ -0,0 +1,46 @@
+package com.example.huertohogar.compo
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun NavegacionPantallas(modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+    NavHost(navController = navController,
+        startDestination = "Login",
+        modifier = modifier) {
+        composable("Login") {
+            FormularioValidacion(
+                loginExitoso = {
+                    navController.navigate("pantalla1")
+                }
+            )
+        }
+        composable("pantalla1") {
+            PantallaUno(onNavigate = { navController.navigate("pantalla2") })
+        }
+        composable("pantalla2") {
+            PantallaDos(onNavigateBack = { navController.popBackStack() })
+        }
+    }
+}
+
+@Composable
+fun PantallaUno(onNavigate: () -> Unit) {
+    Button(onClick = onNavigate) {
+        Text("Ir a Pantalla 2")
+    }
+}
+
+@Composable
+fun PantallaDos(onNavigateBack: () -> Unit) {
+    Button(onClick = onNavigateBack) {
+        Text("Volver a Pantalla 1")
+    }
+}
